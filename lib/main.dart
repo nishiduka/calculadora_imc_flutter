e3884e23bd384966c5a1a3b2c795219a8ee45581
Thomas Nishiduka <thomas.nishiduka@gmail.com>
2023-04-26 20:28:28 -0300

    feat: add full code calc imc

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,130 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Home(),
-  ));
+  runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
 }
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+  const Home({super.key});
+
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  final pesoController = TextEditingController();
-  final alturaController = TextEditingController();
-  String _resultado = '';
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+  String _textoInfo = "Informe seus dados";
 
-  void _resetFields() {
-    pesoController.text = '';
+  final _formIMC = GlobalKey<FormState>();
+
+  void _limpaCampos() {
     alturaController.text = '';
+    pesoController.text = '';
+
     setState(() {
-      _resultado = '';
+      _textoInfo = "Informe seus dados";
     });
   }
 
+  String _getCategoryIMC(double imc) {
+    String baseText = "Obesidade Grau III";
+
+    if (imc < 16.5) {
+      baseText = "Desnutrição";
+    } else if (imc <= 18.5) {
+      baseText = "Abaixo do Peso";
+    } else if (imc <= 24.9) {
+      baseText = "Peso ideal";
+    } else if (imc <= 29.9) {
+      baseText = "Sobrepeso";
+    } else if (imc <= 34.9) {
+      baseText = "Obesidade Grau I";
+    } else if (imc <= 39.9) {
+      baseText = "Obesidade Grau II";
+    }
+
+    return "$baseText (${imc.toStringAsPrecision(3)})";
+  }
+
   void _calcularIMC() {
-    double peso = double.tryParse(pesoController.text) ?? 0.0;
-    double altura = double.tryParse(alturaController.text) ?? 0.0;
-    altura /=100;
-    double imc = peso / (altura * altura);
+    if (!_formIMC.currentState!.validate()) {
+      return;
+    }
+
+    double peso = double.parse(pesoController.text);
+    double altura = double.parse(alturaController.text) / 100;
+    double imc = peso / pow(altura, 2);
+
     setState(() {
-      _resultado = 'Seu IMC é ${imc.toStringAsFixed(2)}';
+      _textoInfo = _getCategoryIMC(imc);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Calculadora IMC'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: <Widget>[
-          IconButton(onPressed: _resetFields, icon: const Icon(Icons.refresh))
-        ],
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            const Icon(Icons.person_outlined, size: 120.0, color: Colors.green),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: 'Peso (KG)',
-                  labelStyle: TextStyle(color: Colors.green)),
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 25.0),
-              controller: pesoController,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: 'Altura (CM)',
-                  labelStyle: TextStyle(color: Colors.green)),
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 25.0),
-              controller: alturaController,
-            ),
-            Padding(
-                padding: const EdgeInsets.only(top: 10, bottom: 10),
-                child: SizedBox(
-                  height: 50.0,
-                  child: ElevatedButton.icon(
-                      icon: const Icon(Icons.send_and_archive_rounded),
-                      style: ElevatedButton.styleFrom(
-                          textStyle: const TextStyle(fontSize: 25.0),
-                          backgroundColor: Colors.green),
-                      onPressed: _calcularIMC,
-                      label: const Text('Calcular')),
-                )),
-            Text(
-              _resultado,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.green, fontSize: 25.0),
-            )
-          ],
-        ),
-      ),
-    );
+        appBar: AppBar(
+            title: const Text('Calculadora IMC'),
+            centerTitle: true,
+            backgroundColor: Colors.green,
+            actions: <Widget>[
+              IconButton(
+                  onPressed: _limpaCampos, icon: const Icon(Icons.refresh))
+            ]),
+        body: SingleChildScrollView(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+            child: Form(
+              key: _formIMC,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    const Icon(Icons.person_outline,
+                        size: 100.0, color: Colors.green),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: 'Peso (KG)',
+                      ),
+                      style: const TextStyle(fontSize: 16.0),
+                      controller: pesoController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'O campo Peso é obrigatório';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: 'Altura (CM)',
+                      ),
+                      style: const TextStyle(fontSize: 16.0),
+                      controller: alturaController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'O campo Altura é obrigatório';
+                        }
+                        return null;
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30, bottom: 20),
+                      child: SizedBox(
+                          height: 40,
+                          child: ElevatedButton(
+                              onPressed: _calcularIMC,
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.green),
+                              child: const Text('Calcular'))),
+                    ),
+                    Text(
+                      _textoInfo,
+                      textAlign: TextAlign.center,
+                    )
+                  ]),
+            )));
   }
 }
